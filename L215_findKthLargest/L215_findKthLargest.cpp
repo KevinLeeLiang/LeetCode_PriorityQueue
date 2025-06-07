f4df0cf0678b9cebc70176ce72a241ba3a43a1f2
garen_lee <srljl@163.com>
2025-06-07 11:42:50 +0800

    重做215,218题

--- L215_findKthLargest/L215_findKthLargest.cpp
+++ L215_findKthLargest/L215_findKthLargest.cpp
@@ -12,18 +12,19 @@
 //
 
 #include "L215_findKthLargest.h"
-void L215_findKthLargest::test(){
-    vector<int>nums;
+
+void L215_findKthLargest::test() {
+    vector<int> nums;
     int k;
-    nums = {3,2,1,5,6,4};
+    nums = {3, 2, 1, 5, 6, 4};
     k = 2;
-    cout<<findKthLargest(nums,k)<<endl;
-    nums = {3,2,3,1,2,4,5,5,6};
+    cout << findKthLargest(nums, k) << endl;
+    nums = {3, 2, 3, 1, 2, 4, 5, 5, 6};
     k = 4;
-    cout<<findKthLargest(nums,k)<<endl;
+    cout << findKthLargest(nums, k) << endl;
 }
 
-void L215_findKthLargest::maxHeapify(vector<int>& a, int i, int heapSize) {
+void L215_findKthLargest::maxHeapify(vector<int> &a, int i, int heapSize) {
     int l = i * 2 + 1, r = i * 2 + 2, largest = i;
     if (l < heapSize && a[l] > a[largest]) {
         largest = l;
@@ -37,13 +38,13 @@ void L215_findKthLargest::maxHeapify(vector<int>& a, int i, int heapSize) {
     }
 }
 
-void L215_findKthLargest::buildMaxHeap(vector<int>& a, int heapSize) {
+void L215_findKthLargest::buildMaxHeap(vector<int> &a, int heapSize) {
     for (int i = heapSize / 2 - 1; i >= 0; --i) {
         maxHeapify(a, i, heapSize);
     }
 }
 
-int L215_findKthLargest::findKthLargest(vector<int>& nums, int k) {
+int L215_findKthLargest::findKthLargest(vector<int> &nums, int k) {
     int heapSize = nums.size();
     buildMaxHeap(nums, heapSize);
     for (int i = nums.size() - 1; i >= nums.size() - k + 1; --i) {
--- L218_getSkyline/L218_getSkyline.cpp
+++ L218_getSkyline/L218_getSkyline.cpp
@@ -13,29 +13,28 @@
 
 #include "L218_getSkyline.h"
 vector<vector<int>> L218_getSkyline::getSkyline(vector<vector<int>> &buildings) {
-    auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) -> bool { return a.second < b.second; };
-    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> que(cmp);
-
-    vector<int> boundaries;
-    for (auto& building : buildings) {
-        boundaries.emplace_back(building[0]);
-        boundaries.emplace_back(building[1]);
+    auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) {
+        return a.second < b.second;
+    };
+    priority_queue <pair<int, int>, vector<pair<int, int>>, decltype(cmp)> que(cmp);
+    vector<int>boundaries;
+    for (auto &building : buildings) {
+        boundaries.push_back(building[0]);
+        boundaries.push_back(building[1]);
     }
     sort(boundaries.begin(), boundaries.end());
-
     vector<vector<int>> ret;
-    int n = buildings.size(), idx = 0;
-    for (auto& boundary : boundaries) {
-        while (idx < n && buildings[idx][0] <= boundary) {
-            que.emplace(buildings[idx][1], buildings[idx][2]);
+    int idx = 0;
+    for (auto &boundary : boundaries) {
+        while (idx < buildings.size() && buildings[idx][0] <= boundary) {
+            que.push(make_pair(-buildings[idx][2], buildings[idx][1]));
             idx++;
         }
-        while (!que.empty() && que.top().first <= boundary) {
+        while (!que.empty() && que.top().second <= boundary) {
             que.pop();
         }
-
         int maxn = que.empty() ? 0 : que.top().second;
-        if (ret.size() == 0 || maxn != ret.back()[1]) {
+        if (ret.empty() || ret.back()[1] != maxn) {
             ret.push_back({boundary, maxn});
         }
     }
